20a6c09004ed373e27fd72942aabb35542fbe14f
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-11 18:34:35 +0200

    javascript-redo-update

--- index.js
+++ index.js
@@ -7,34 +7,54 @@ const establishConnection = () => {
     };
     xhttp.open("GET", "terc.xml", true);
     xhttp.send();
-};
+
+}
+
 const xmlActionGroup = (xml) => {
+
     const execute = (path) => {
         let output = "";
         if (xml.evaluate) {
             const nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
             let result = nodes.iterateNext();
             while (result) {
-                console.log(result);
-                output += result.childNodes[0].nodeValue + '<br />';
-                result = nodes.iterateNext();
+                console.log(result)
+                output += result.childNodes[0].nodeValue + '<br />'
+                result = nodes.iterateNext()
             }
         }
+
         document.querySelector('#test').innerHTML = output;
-        console.log(output);
-    };
+        console.log(output)
+    }
+
     // małopolskie
     // execute("//row[(NAZWA_DOD='miasto' or NAZWA_DOD='gmina miejska') and WOJ='12']/NAZWA")
+
     // cała Polska
     // execute("//row[(NAZWA_DOD='miasto' or NAZWA_DOD='gmina miejska')]/NAZWA")
-    const city = document.querySelector('#city');
+
+    const city = document.querySelector('#city')
     city.addEventListener('input', updateResults);
+
     function updateResults(e) {
         let content = e.target.value;
-        content.toLowerCase();
+        content.toLowerCase()
         content = content.toString().charAt(0).toUpperCase() + content.toString().slice(1);
-        console.log(content);
-        execute("//row[starts-with(NAZWA,'" + content.toString() + "') and ((starts-with(NAZWA_DOD,'miasto') or NAZWA_DOD='miasto') or NAZWA_DOD='gmina miejska')]/NAZWA ");
+        console.log(content)
+
+        execute("//row[starts-with(NAZWA,'" + content.toString() + "') and ((starts-with(NAZWA_DOD,'miasto') or NAZWA_DOD='miasto') or NAZWA_DOD='gmina miejska')]/NAZWA ")
     }
-};
-establishConnection();
+
+
+
+}
+
+
+
+establishConnection()
+
+
+
+
+
