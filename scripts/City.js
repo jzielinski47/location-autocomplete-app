788e1eb22b4da0b38f1639c057ff1b511921ffeb
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-17 16:14:58 +0200

    script-update

--- scripts/City.js
+++ scripts/City.js
@@ -1,18 +1,43 @@
 class City {
-    constructor(name) {
-        this.name = name;
-        this.county = county;
-        this.voivodeship = this.searchVoivodeshipName;
+    constructor(name, xml) {
+        this.xml = xml;
+
+        this.name = name.toString();
+        this.voivodeship = this.searchVoivodeshipName(`//row[(RODZ='1' or RODZ='3') and NAZWA='${this.name}']/WOJ`);
+        this.county = this.searchVoivodeshipName(`//row[(RODZ='1' or RODZ='3') and NAZWA='${this.name}' and WOJ='${this.voivodeship}']/POW`);
+
+        this.vname;
     }
 
     searchVoivodeshipName = (path) => {
-        if (xml.evaluate) {
-            const nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+        if (this.xml.evaluate) {
+            const nodes = this.xml.evaluate(path, this.xml, null, XPathResult.ANY_TYPE, null);
             let result = nodes.iterateNext();
             while (result) {
+                // console.log(result)
                 return result.childNodes[0].nodeValue
                 result = nodes.iterateNext()
             }
         }
+    }
+
+    searchCountyName = (path) => {
+        if (this.xml.evaluate) {
+            const nodes = this.xml.evaluate(path, this.xml, null, XPathResult.ANY_TYPE, null);
+            let result = nodes.iterateNext();
+            while (result) {
+                // console.log(result)
+                return result.childNodes[0].nodeValue
+                result = nodes.iterateNext()
+            }
+        }
+    }
+
+    rereadNames = () => {
+
+    }
+
+    display = () => {
+        document.querySelector('#dist').innerHTML += `${this.name}, ${this.voivodeship}, ${this.county} <br>`
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -17,23 +17,43 @@ const xmlActionGroup = (xml) => {
     const city = document.querySelector('#city')
     const dist = document.querySelector('#dist')
 
+    const search = path => {
+        if (xml.evaluate) {
+            const nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+            let result = nodes.iterateNext();
+            let i = 1;
+            while (result) {
+                console.log(i, result.childNodes[0].nodeValue)
+                result = nodes.iterateNext(); i++;
+            }
+        }
+    }
+
+    const searchCityName = path => {
+        if (xml.evaluate) {
+            const nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+            let result = nodes.iterateNext();
+            while (result) {
+                console.log(result.childNodes[0].nodeValue)
+                let city = new City(result.childNodes[0].nodeValue, xml)
+                city.display()
+                result = nodes.iterateNext();
+            }
+        }
+    }
+
     city.addEventListener('input', event => {
         let content = event.target.value;
         content.toLowerCase();
         content = content.toString().charAt(0).toUpperCase() + content.toString().slice(1);
         console.log('on-input:', content)
-    });
 
-    const search = path => {
-        if (xml.evaluate) {
-            const nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
-            let result = nodes.iterateNext();
-            while (result) {
-                return result.childNodes[0].nodeValue
-                result = nodes.iterateNext()
-            }
+        if (content.length > 0) {
+            dist.innerHTML = ''
+            searchCityName(`//row[(RODZ='1' or RODZ='3') and starts-with(NAZWA,'${content}')]/NAZWA`)
         }
-    }
+
+    });
 
 
     // małopolskie 
